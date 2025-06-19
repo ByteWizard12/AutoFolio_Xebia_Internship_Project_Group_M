2fd9dfe9f2cd2f14a9a4d38b06c933c320966641
Harsh <harshyadav12012004@gmail.com>
2025-06-19 22:17:37 +0530

    Added mongodb database connection

---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+app.use(express.json());
+
+async function main() {
+try{
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Database connected")
+    app.listen(3000 , ( ) => {
+        console.log("Server is running on : https://localhost:3000");
+    })
+}
+catch(e){
+    console.log("Failed to connect to database.\nTry again....!")
+    console.log(e);
+}
+}
+
+main();
